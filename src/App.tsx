5ff9e59596bac3db95f7ffbc1cc89551cded0388
Halcyon666 <whalefallcl@gmail.com>
2025-09-24 20:56:44 +0800

    Custom Components Part 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Hello from "./Hello";
+import Hello from "./testcomponent/Hello";
 
 // import { createElement } from "react";
 
