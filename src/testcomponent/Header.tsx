a931fa0220df026c3a2180be390b2841b2c437cb
Halcyon666 <whalefallcl@gmail.com>
2025-09-26 00:10:43 +0800

    change the header like this https://s2.loli.net/2025/09/26/JNE8I27jst5gq3Y.png

--- src/testcomponent/Header.tsx
+++ src/testcomponent/Header.tsx
@@ -1,10 +1,19 @@
 import reactLogo from "../assets/react.svg";
+// 不要忘记导入 不然css没有效果
+import "./index.css";
 
 export default function Header() {
   return (
     <>
-      <header>
+      <header className="header">
         <img src={reactLogo} width="40px" alt="React Logo" />
+        <nav>
+          <ul className="nav-list">
+            <li className="nav-list-item">Pricing</li>
+            <li className="nav-list-item">About</li>
+            <li className="nav-list-item">Contact</li>
+          </ul>
+        </nav>
       </header>
     </>
   );
---
+++ src/testcomponent/readme.md
@@ -0,0 +1,5 @@
+# commit
+
+## header css
+
+![20250926000929](https://s2.loli.net/2025/09/26/JNE8I27jst5gq3Y.png)
