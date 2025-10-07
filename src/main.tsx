05dc8b1f13e29d3afdae075fabc1939299f6e748
Halcyon666 <whalefallcl@gmail.com>
2025-10-07 17:56:26 +0800

    reset css in challenge4

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./challenge4.2/App";
+import App from "./challenge4/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
