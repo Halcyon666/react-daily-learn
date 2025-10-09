337973860054566ef49281e7e127b097dadb8f2e
Halcyon666 <whalefallcl@gmail.com>
2025-10-09 22:26:19 +0800

    get value from input element

--- src/Chef Claude/Main.tsx
+++ src/Chef Claude/Main.tsx
@@ -1,14 +1,40 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  // ============================
+  // tips this changes will not re-render by react.
+  // page render a new url. so prevent it
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    // the key is the name of input
+    const newIngredient = formData.get("ingredient");
+    if (typeof newIngredient === "string" && newIngredient?.trim() !== "") {
+      ingredients.push(newIngredient.trim());
+      console.log(ingredients);
+    } else {
+      console.warn("No ingredient provided");
+    }
+
+    console.log(ingredients);
+  };
+
   return (
     <main>
-      <form id="main-form" className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
+          name="ingredient"
           aria-label="Add ingredient"
           placeholder="e.g. oregano"
         ></input>
         <button>Add ingredient</button>
       </form>
+      <h1>Ingrredients on hand:</h1>
+      <ul className="ingredient-ul">
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
     </main>
   );
 }
