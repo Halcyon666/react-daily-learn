e85f28be43086cd2b799a3d5293464dc5d86fc56
Halcyon666 <whalefallcl@gmail.com>
2025-10-12 17:52:46 +0800

    using ternary instead of &&  to dynimically display elements

--- src/joke/Joke.tsx
+++ src/joke/Joke.tsx
@@ -5,13 +5,12 @@ export default function Joke({ setup, punchline }: JokeInfo) {
   const [isShow, setIsShow] = useState(false);
   const changeVisible = () => {
     setIsShow((prev) => !prev);
-    console.log(isShow);
   };
 
   return (
     <>
       {setup && <h3 className="setup">setup: {setup}</h3>}
-      {isShow && <p>punchline: {punchline}</p>}
+      {isShow ? <p>punchline: {punchline}</p> : null}
       {/*       {!isShow && <button onClick={changeVisible}>show punchline</button>}
       {isShow && <button onClick={changeVisible}>hide punchline</button>} */}
       <button onClick={changeVisible}>
