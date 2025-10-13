c8a004ed9c956ccda7d793aac32f8a3834a513b0
Halcyon666 <whalefallcl@gmail.com>
2025-10-13 23:47:09 +0800

    pad toggle

--- src/sound pads challenge/Pad.tsx
+++ src/sound pads challenge/Pad.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import type { PadInfo } from "./pads";
 
 export default function Pad({ color, on }: PadInfo) {
+  // import hold another status in Pad component. instead of using original in parent component
+  // but this is a derived state
+  const [innerOn, setInnerOn] = useState(on);
+
+  const toggle = () => {
+    setInnerOn((prev) => !prev);
+  };
   return (
     <button
+      onClick={toggle}
       style={{ backgroundColor: color }}
-      className={on ? "on" : undefined}
+      className={innerOn ? "on" : undefined}
     ></button>
   );
 }
