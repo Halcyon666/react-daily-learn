bebf0a5bd5864b9f3e1b489d1cca1240e9c00ca5
Halcyon666 <whalefallcl@gmail.com>
2025-10-17 23:01:09 +0800

    use effect quiz

--- src/use effect/App.tsx
+++ src/use effect/App.tsx
@@ -3,18 +3,23 @@ import "./index.css";
 
 export default function App() {
   const [starWarsData, setStarWarsData] = React.useState({});
+  const [count, setCount] = React.useState(1);
 
   console.log("Rendered!");
 
   React.useEffect(() => {
     console.log("useEffect runs");
-    fetch("https://swapi.dev/api/people/1")
+    fetch(`https://swapi.dev/api/people/${count}`)
       .then((res) => res.json())
       .then((data) => setStarWarsData(data));
-  }, []);
+  }, [count]);
 
   return (
     <div>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Get next Character
+      </button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
