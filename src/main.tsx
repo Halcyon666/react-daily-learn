f76510dabbc9bc6ad7267441ac1150e5bed1c9ac
Halcyon666 <whalefallcl@gmail.com>
2025-10-18 23:23:35 +0800

    my css tenzies

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
-import App from "./chef claude/App";
+import App from "./tenzies/App";
 
 createRoot(document.getElementById("root")!).render(<App />);
---
+++ src/tenzies/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App: React.FC = () => {
+  return (
+    <main className="main">
+      <div></div>
+    </main>
+  );
+};
+
+export default App;
