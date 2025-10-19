43dc0f75ef80496bc3d94f01378192138ff2ca9b
Halcyon666 <whalefallcl@gmail.com>
2025-10-19 16:42:02 +0800

    add rollDice button

--- src/tenzies/App.tsx
+++ src/tenzies/App.tsx
@@ -3,16 +3,21 @@ import "./index.css";
 import Die from "./Die";
 
 const App: React.FC = () => {
-  const [dice, setDice] = useState<number[]>(() =>
-    Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6))
-  );
+  const generateDice = () =>
+    Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6));
+  const [dice, setDice] = useState<number[]>(generateDice());
 
   const diceElements = dice.map((value, key) => (
     <Die key={key} value={value} />
   ));
+
+  const rollDice = () => {
+    setDice(generateDice());
+  };
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
+      <button onClick={rollDice}>Roll Dice</button>
     </main>
   );
 };
