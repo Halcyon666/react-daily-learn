464e46f0c0caa8b5c94ae9be3aac8338682bb5c1
Halcyon666 <whalefallcl@gmail.com>
2025-10-31 19:50:06 +0800

    my version of only display correctly guessed letters in word

--- src/assembly endgame/App.tsx
+++ src/assembly endgame/App.tsx
@@ -5,10 +5,7 @@ import clsx from "clsx";
 
 export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = useState("react");
-  const currentWordElements = currentWord.split("").map((character, index) => (
-    // no change the array so use index directly
-    <span key={index}>{character.toUpperCase()}</span>
-  ));
+
   const alphabet = "qwertyuiopasdfghjkl";
 
   const alphabet1 = "zxcvbnm";
@@ -36,7 +33,16 @@ export default function AssemblyEndgame() {
     });
   };
 
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const arr = Array(currentWord.length).fill("");
+  // const arr = Array.from({ length: currentWord.length }, () => "");
+  const [guessedLetters, setGuessedLetters] = useState<string[]>(arr);
+  const currentWordElements = guessedLetters.map(
+    (character, index) =>
+      // no change the array so use index directly
+      currentWord.includes(character) && (
+        <span key={index}>{character.toUpperCase()}</span>
+      )
+  );
 
   const keyboardClick = (letter: string) => {
     setGuessedLetters((prevLetters) =>
